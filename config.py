246d27b439cc5d41bfbad53d62e50350e955b0ef
erikatem <erikatem86@gmail.com>
2022-01-11 19:38:09 -0600

    easy-project

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee23.cnsi1myyo5rp.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee23"
+custombucket = "employee123"
+customregion = "us-east-1"
+
